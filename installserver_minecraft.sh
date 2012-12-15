f5de5e1b1d835cddd4e932af6552ab2de23d2b00
InfoWiki <contact@mickael-stanislas.com>
2012-12-15 22:56:44 +0100

    Création du fichier

---
+++ installserver_minecraft.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Script dev. par Mickael Stanislas
+# http://mickael-stanislas.com/
+# Script Open-Source 
+
+# Definition de la variable
+URL='http://dl.bukkit.org/downloads/craftbukkit/get/01434_1.3.2-R1.0/craftbukkit.jar'
+
+# Debut de l'installation
+echo "Installation serveur Minecraft"
+echo "Debut du script" > $LOG_FILE
+echo "Création de l'utilisateur minecraft"
+useradd -m minecraft
+echo "Definition du mot de passe de l'utilisateur minecraft"
+passwd minecraft
+clear 
+echo "Connexion avec l'utilisateur minecraft"
+su minecraft
+#Modification Prompte Bash
+cd
+echo "PS1='\[\e[0;32m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \[\e[1;32m\]\$\[\e[m\] \[\e[1;37m\]'" >> .bashrc
+bash
+# Ajout des sources
+echo "# non-free" >> /etc/apt/sources.list
+echo deb http://www.duinsoft.nl/pkg debs all >> /etc/apt/source
+# Ajout des souces
+apt-key adv --keyserver keys.gnupg.net --recv-keys 5CB26B26
+add-apt-repository ppa:arnaud-hartmann/glances-stable
+apt-get update
+# Installation des programmes
+apt-get install zip unzip screen htop glances update-sun-jre
+
+# Installation minecraft 
+cd /home/minecraft/
+wget $URL
+mv craft*.jar craftbukkit.jar
+
+# Installation du script de démarrage
+cd /etc/init.d/
+wget http://infowiki.fr/download/minecraft/minecraft.txt
+mv minecraft.txt minecraft
+chmod +x minecraft
+
+# FIN ... END
+echo "Fin du script" >> $LOG_FILE
+
